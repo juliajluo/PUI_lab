dce7e3454729a754f9a5ebd518f4425ead2e2534
Julia Luo <julialuo@andrew.cmu.edu>
2018-10-24 02:25:20 -0400

    product page update works

--- homework_6/scripts.js
+++ homework_6/scripts.js
@@ -1,7 +1,7 @@
 function bunDetails(name, price, image, glazing) {
   this.name = name;
   this.price = price;
-  this.image = "roll.png";
+  this.image = image;
   this.glazing = glazing;
   // this.type = "Original";
 }
@@ -12,34 +12,41 @@ function bunDetails(name, price, image, glazing) {
 function getProductDetails(clickedProduct) {
   // clickedProduct = 
   if (clickedProduct == "Original") {
-    return new bunDetails("The Original", "$12.0", "original.png", "Default");
+    return new bunDetails("The Original", "$12.0", "images/original.png", "Default");
   }
 
   if (clickedProduct == "Raspberry") {
-    return new bunDetails("The Raspberry", "$15.00", "raspberry.png", "Default");
+    return new bunDetails("The Raspberry", "$15.00", "images/raspberry.png", "Default");
   }
 
   if (clickedProduct == "Chocolate") {
-    return new bunDetails("The Chocolate", "$5.00", "chocolate.png", "Default");
+    return new bunDetails("The Chocolate", "$5.00", "images/chocolate.png", "Default");
   }
 
   // return item
 }
 
+
+
 // first get the product element
 // then when one is clicked, use getProductDetails to produce details 
 $(document).ready(function() {
   // var product = document.getElementById("product");
+  var item = JSON.parse(localStorage.getItem("savedBun"));
+  $("#bun-name").html(item.name);
+  $("#bun-price").text(item.price);
+  $("#bun-image").attr("src", item.image);
+  // $("#animal-img").attr("src", animal.image);
 
-  $("#product").click(function() {
-    var item = getProductDetails();
+  function populateItemPage(event) {
+    var item = getProductDetails(event.data.name);
+    localStorage.setItem("savedBun", JSON.stringify(item));
 
-    console.log(item.name)
+    console.log(item)
+  }
 
-    $("#bun-name").text(item.name);
-    $("#bun-price").text(item.price);
-    $("#bun-price").text("18.00");
-
-    $("#bun-image").attr("src", item.image);
-  });
+  $("#original").click({name: "Original"}, populateItemPage);
+  $("#raspberry").click({name: "Raspberry"}, populateItemPage);
+  $("#raspberry").click({name: "Raspberry"}, populateItemPage);
 });
+
