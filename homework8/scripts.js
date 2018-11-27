00ea1b679f666494bddb9f4869293441d9611bc6
Julia Luo <julialuo@andrew.cmu.edu>
2018-11-27 00:38:59 -0500

    responsive grid

--- homework8/scripts.js
+++ homework8/scripts.js
@@ -6,7 +6,17 @@
 		    $('.left-navigation').toggleClass('is-active');
 		  });
 
-        $("#sidebar").stick_in_parent();
+        // $("#sidebar").stick_in_parent();
+
+        $('#intro').flowtype({
+             minimum   : 50,
+             maximum   : 120,
+             minFont   : 12,
+             maxFont   : 40,
+             fontRatio : 30
+        });
+
+
 
    });
 
