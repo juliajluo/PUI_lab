5b67128de1daada297591051371001232aec6515
Julia Luo <julialuo@andrew.cmu.edu>
2018-11-28 12:58:49 -0500

    started preloader

--- homework8/scripts.js
+++ homework8/scripts.js
@@ -1,10 +1,42 @@
- $(document).ready(function(){                    
+ 
+
+var timeout;
+
+function myFunction() {
+
+    timeout = setTimeout(showPage, 8000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+
+  document.getElementById("container").style.display = "initial";
+  document.body.style.background = "white"
+
+  //do this in jquery soon
+}
 
 
-        $('#nav-toggle').click(function(){
-		    $('#nav-toggle').toggleClass('is-active');
-		    $('.left-navigation').toggleClass('is-active');
-		  });
+
+
+
+
+
+ $(document).ready(function(){ 
+    //     var timeout;
+    //     timeout = setTimeout(showPage, 2000);
+
+        
+    //     jQuery(window).load(function() {
+    //         $( '#loader' ).fadeOut();
+    //         $('#container').fadeIn();
+    //     });
+
+
+    //     $('#nav-toggle').click(function(){
+		  //   $('#nav-toggle').toggleClass('is-active');
+		  //   $('.left-navigation').toggleClass('is-active');
+		  // });
 
         // $("#sidebar").stick_in_parent();
 
@@ -16,10 +48,5 @@
              fontRatio : 30
         });
 
-
-
-   });
-
-
-
+});
 
